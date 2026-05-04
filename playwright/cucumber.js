f056d2dc76737dbe5249ce00e42dd17eb0682499
=shraddhasharma26 <shraddhasharma6991@gmail.com>
2026-05-04 16:50:19 +0530

    Remove unwanted files outside anime_api_project - keep only anime_api_project content

--- playwright/cucumber.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  default: {
-    require: ['tests/hooks/**/*.js', 'tests/step_defination/**/*.js'],
-    format: ['progress-bar', 'html:playwright-report/cucumber-report.html'],
-    paths: ['tests/features/**/*.feature']
-  }
-};
--- playwright/tests/features/login.feature
+++
@@ -1,6 +0,0 @@
-Feature: Successful Login
-
-  Scenario: User logs in successfully
-    Given user goes to url
-    When user clicks on login
-    Then user is successfully login
